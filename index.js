bed725591a555e0a96d1ccfcd6f00dfa15ddc288
Markus <markus.wagner@zoi.de>
2022-06-23 08:19:23 +0200

    adjustments

---
+++ index.js
@@ -0,0 +1,6 @@
+import icloud from "./src/icloud.js"
+
+;(async () => {
+  const res = await icloud()
+  console.log(res)
+})()
--- src/icloud.js
+++ src/icloud.js
@@ -1,51 +1,12 @@
-import ICloud from "apple-icloud"
-import dotenv from "dotenv"
-import readline from "readline"
-import { stdin as input, stdout as output } from "node:process"
-const rl = readline.createInterface({ input, output })
+import promptiCloud from './prompt-credentials.js'
+// import dotenv from "dotenv"
 
-// An empty session. Has to be a session object or file path that points on a JSON file containing your session
-dotenv.config()
-export default cli => {
-  return new Promise((resolve, reject) => {
-    const session = {}
-    const myCloud = new ICloud(
-      "./session.json",
-      process.env.ICLOUD_USERNAME,
-      process.env.ICLOUD_PASSWORD
-    )
+(async () => {
+  // Login to icloud and ask for new credentials if needed
+  const myCloud = await promptiCloud()
 
-    const result = {
-      handler: myCloud,
-      session,
-      async getReminders(open = true) {
-        let tasks = []
-        const state = open ? "open" : "completed"
-        console.log(`Retrieving ${state} remindaers`)
-        if (open) {
-          tasks = await myCloud.Reminders.getOpenTasks()
-        } else {
-          tasks = await myCloud.Reminders.getCompletedTasks()
-        }
-        console.log("Success", tasks)
-        return tasks
-      },
-    }
-    
-    rl.question("Please enter a 2FA Code for iCloud: ", twoFactorCode => {
-      myCloud.securityCode = twoFactorCode
-      myCloud.login(process.env.ICLOUD_USERNAME, process.env.ICLOUD_PASSWORD, (res) => {
-        console.log(res);
-        myCloud.saveSession()
-        resolve(result);
-      })
-      // myCloud.on("ready", () => {
-      //   resolve(result)
-      // })
-      myCloud.on("error", reject)
-      myCloud.on("err", reject)
-    })
+  const collectionsWithOpenTasks = await myCloud.Reminders.getOpenTasks()
 
-    return myCloud
-  })
-}
+  console.log(collectionsWithOpenTasks)
+  return collectionsWithOpenTasks
+})()
